8e75d92f4709b5438ec1b5b01a2931c3e5b0f480
Alzter Aquino <alztr2908@gmail.com>
2024-05-29 01:47:17 +0800

    add GlobalDeclaration.h forgot to add before

---
+++ src/GlobalDeclarations.h
@@ -0,0 +1,12 @@
+#ifndef GLOBALDECLARATIONS_H
+#define GLOBALDECLARATIONS_H
+
+#include "./Utils/LCDManager.h"
+#include "./Utils/PageManager.h"
+// #include "./Thermocycler/ThermocyclerArray.h"
+
+extern LCDManager lcd;
+extern PageManager pageManager;
+// extern ThermocyclerArray thermocyclerArray;
+
+#endif
